da781b8d7917f80c0a9192609b8cb90abb4cf843
manuk <mahhov1@gmail.com>
2019-03-29 18:08:55 -0700

    global shortcut key (ctrl shift c) to open. dont quit on window close, allow tray menu to quit.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const {app} = require('electron');
 const {ViewHandle, TrayHelper, ShortcutListener, appReadyPromise} = require('js-desktop-base');
 
 TrayHelper.createExitTray('resources/calculator-solid.png', 'calculator');
@@ -10,10 +11,22 @@ class MyViewHandle extends ViewHandle {
 			height: 500,
 			frame: false,
 			thickFrame: false,
-			show: true,
+			show: false,
 			webPreferences: {nodeIntegration: true}
 		}, path.join(__dirname, './view.html'));
-		// ShortcutListener.add('Control+Shift+Z', this.show.bind(this));
+		ShortcutListener.add('Control+Shift+C', this.show.bind(this));
+		this.setupHideInsteadOfClose();
+	}
+
+	async setupHideInsteadOfClose() {
+		(await this.window).on('close', ev => {
+			if (!this.quitting) {
+				ev.preventDefault();
+				this.hide();
+			}
+		});
+
+		app.on('before-quit', () => this.quitting = true);
 	}
 
 	async onMessage(message) {
