a8eedb59c900aaf6b5e158664280aa6270427bac
manuk <mahhov1@gmail.com>
2019-03-29 19:12:02 -0700

    arrow up/down to select previous rows, esc to clear selection if selected else clear input, enter to copy selection if selected else evaluate input. clear selection on input

--- src/view.js
+++ src/view.js
@@ -21,6 +21,7 @@ class Outputs {
 		for (let i = 0; i < size; i++)
 			$('#output').appendChild(Outputs.createRowEl());
 		this.clear();
+		this.clearSelected();
 	}
 
 	static createRowEl() {
@@ -39,7 +40,7 @@ class Outputs {
 	}
 
 	clear() {
-		this.valuePairs = Array(this.size).fill([]);
+		this.valuePairs = Array(this.size).fill(['']);
 		this.updateView();
 	}
 
@@ -49,18 +50,44 @@ class Outputs {
 		this.updateView();
 	}
 
-	updateView() {
-		this.valuePairs.forEach((valuePair, i) => {
-			let row = $('#output').children[i];
-			$$(row, '.left').textContent = valuePair[0];
-			$$(row, '.right').textContent = valuePair[1] ? '= ' + valuePair[1] : '';
-		});
-	}
-
 	get prevResults() {
 		return this.valuePairs
 			.map(a => a[1])
 			.reverse();
+	}
+
+	clearSelected() {
+		this.selected = this.size;
+		this.updateView();
+	}
+
+	selectUp() {
+		if (--this.selected < 0)
+			this.clearSelected();
+		this.updateView();
+	}
+
+	selectDown() {
+		if (++this.selected > this.size)
+			this.selected = 0;
+		this.updateView();
+	}
+
+	get hasSelected() {
+		return this.selected !== this.size;
+	}
+
+	get selectedLeft() {
+		return this.valuePairs[this.selected][0];
+	}
+
+	updateView() {
+		this.valuePairs.forEach((valuePair, i) => {
+			let row = $('#output').children[i];
+			row.classList.toggle('selected', i === this.selected);
+			$$(row, '.left').textContent = valuePair[0];
+			$$(row, '.right').textContent = valuePair[0] ? '= ' + valuePair[1] : '';
+		});
 	}
 }
 
@@ -68,32 +95,44 @@ let outputs = new Outputs(10);
 
 let alwaysOnTop;
 
-document.body.addEventListener('keydown', ({altKey, ctrlKey, shiftKey, code}) => {
+document.body.addEventListener('keydown', ev => {
+	let {altKey, ctrlKey, shiftKey, code} = ev;
 	$('#input').focus();
+
 	switch (code) {
 		case 'Enter':
 			if (altKey) {
 				alwaysOnTop = !alwaysOnTop;
 				$('html').classList.toggle('always-on-top', alwaysOnTop);
 				ipcSend({name: 'sticky', value: alwaysOnTop});
-			} else {
+			} else if (outputs.hkasSelected)
+				$('#input').value = outputs.selectedLeft;
+			else {
 				let input = $('#input').value;
 				outputs.push([input, Calc.do(input, outputs.prevResults)]);
 			}
 			break;
 
 		case 'ArrowUp':
+			outputs.selectUp();
+			ev.preventDefault();
+			break;
 		case 'ArrowDown':
-			if (ctrlKey)
-				;
+			ev.preventDefault();
+			outputs.selectDown();
 			break;
 
 		case 'Escape':
 			if (shiftKey)
 				outputs.clear();
+			else if (outputs.hasSelected)
+				outputs.clearSelected();
 			else
 				$('#input').value = '';
 			break;
+
+		default:
+			outputs.clearSelected();
 	}
 });
 
